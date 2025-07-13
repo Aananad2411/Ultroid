3463b080d1427d25ff592efd455028294f1abca0
Aananad2411 <aanandsahu127@gmail.com>
2025-07-13 13:59:53 +0530

    Update _assistant.py

--- pyUltroid/_misc/_assistant.py
+++ pyUltroid/_misc/_assistant.py
@@ -1,138 +1,117 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2025 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
+Anu - UserBot
 
-import inspect
-import re
-from traceback import format_exc
+Modified from Ultroid - UserBot
 
-from telethon import Button
-from telethon.errors import QueryIdInvalidError
-from telethon.events import CallbackQuery, InlineQuery, NewMessage
-from telethon.tl.types import InputWebDocument
+Original License: GNU AGPL v3.0
 
-from .. import LOGS, asst, udB, ultroid_bot
-from ..fns.admins import admin_check
-from . import append_or_update, owner_and_sudos
+import inspect import re from traceback import format_exc
 
-OWNER = Anu_bot.full_name
+from telethon import Button from telethon.errors import QueryIdInvalidError from telethon.events import CallbackQuery, InlineQuery, NewMessage from telethon.tl.types import InputWebDocument
 
-MSG = f"""
-**Anu - UserBot**
-➖➖➖➖➖➖➖➖➖➖
-**Owner**: [{OWNER}](tg://user?id={ultroid_bot.uid})
-➖➖➖➖➖➖➖➖➖➖
-"""
+from .. import LOGS, asst, udB, ultroid_bot from ..fns.admins import admin_check from . import append_or_update, owner_and_sudos
 
+OWNER = ultroid_bot.full_name
 
+MSG = f""" Anu - UserBot ➖➖➖➖➖➖➖➖➖➖ Owner: {OWNER} ➖➖➖➖➖➖➖➖➖➖ """
 
-# decorator for assistant
+IN_BTTS = [ [Button.url("🌐 Support", "https://t.me/SarkarTerminal")], [Button.inline("✨ Help", data=b"help")], ]
 
+decorator for assistant
 
-def asst_cmd(pattern=None, load=None, owner=False, **kwargs):
-    """Decorator for assistant's command"""
-    name = inspect.stack()[1].filename.split("/")[-1].replace(".py", "")
-    kwargs["forwards"] = False
+def asst_cmd(pattern=None, load=None, owner=False, **kwargs): """Decorator for assistant's command""" name = inspect.stack()[1].filename.split("/")[-1].replace(".py", "") kwargs["forwards"] = False
 
-    def ult(func):
-        if pattern:
-            kwargs["pattern"] = re.compile(f"^/{pattern}")
+def ult(func):
+    if pattern:
+        kwargs["pattern"] = re.compile(f"^/{pattern}")
 
-        async def handler(event):
-            if owner and event.sender_id not in owner_and_sudos():
-                return
-            try:
-                await func(event)
-            except Exception as er:
-                LOGS.exception(er)
+    async def handler(event):
+        if owner and event.sender_id not in owner_and_sudos():
+            return
+        try:
+            await func(event)
+        except Exception as er:
+            LOGS.exception(er)
 
-        asst.add_event_handler(handler, NewMessage(**kwargs))
-        if load is not None:
-            append_or_update(load, func, name, kwargs)
+    asst.add_event_handler(handler, NewMessage(**kwargs))
+    if load is not None:
+        append_or_update(load, func, name, kwargs)
 
-    return ult
+return ult
 
+def callback(data=None, from_users=None, admins=False, owner=False, **kwargs): """Assistant's callback decorator""" if from_users is None: from_users = [] if "me" in from_users: from_users.remove("me") from_users.append(ultroid_bot.uid)
 
-def callback(data=None, from_users=[], admins=False, owner=False, **kwargs):
-    """Assistant's callback decorator"""
-    if "me" in from_users:
-        from_users.remove("me")
-        from_users.append(ultroid_bot.uid)
+def ultr(func):
+    async def wrapper(event):
+        if admins and not await admin_check(event):
+            return
+        if from_users and event.sender_id not in from_users:
+            return await event.answer("Not for You!", alert=True)
+        if owner and event.sender_id not in owner_and_sudos():
+            return await event.answer(f"This is {OWNER}'s bot!!")
+        try:
+            await func(event)
+        except Exception as er:
+            LOGS.exception(er)
 
-    def ultr(func):
-        async def wrapper(event):
-            if admins and not await admin_check(event):
-                return
-            if from_users and event.sender_id not in from_users:
-                return await event.answer("Not for You!", alert=True)
-            if owner and event.sender_id not in owner_and_sudos():
-                return await event.answer(f"This is {OWNER}'s bot!!")
-            try:
-                await func(event)
-            except Exception as er:
-                LOGS.exception(er)
+    asst.add_event_handler(wrapper, CallbackQuery(data=data, **kwargs))
 
-        asst.add_event_handler(wrapper, CallbackQuery(data=data, **kwargs))
+return ultr
 
-    return ultr
+def in_pattern(pattern=None, owner=False, **kwargs): """Assistant's inline decorator."""
 
-
-def in_pattern(pattern=None, owner=False, **kwargs):
-    """Assistant's inline decorator."""
-
-    def don(func):
-        async def wrapper(event):
-            if owner and event.sender_id not in owner_and_sudos():
-                res = [
-                    await event.builder.article(
-                        title=" Anu Userbot",
-                        text=MSG,
-                        thumb=InputWebDocument(
-                            "https://graph.org/file/04fbe1479fa10209c0664-6feb1a7ffe75d4673f.jpg",
-                            0,
-                            "image/jpeg",
-                            [],
-                        ),
-                        buttons=IN_BTTS,
-                    )
-                ]
-                return await event.answer(
-                    res,
-                    switch_pm=f"🤖: Assistant of {OWNER}",
-                    switch_pm_param="start",
+def don(func):
+    async def wrapper(event):
+        if owner and event.sender_id not in owner_and_sudos():
+            res = [
+                await event.builder.article(
+                    title=" Anu Userbot",
+                    text=MSG,
+                    thumb=InputWebDocument(
+                        "https://graph.org/file/04fbe1479fa10209c0664-6feb1a7ffe75d4673f.jpg",
+                        0,
+                        "image/jpeg",
+                        [],
+                    ),
+                    buttons=IN_BTTS,
                 )
+            ]
+            return await event.answer(
+                res,
+                switch_pm=f"🤖: Assistant of {OWNER}",
+                switch_pm_param="start",
+            )
+        try:
+            await func(event)
+        except QueryIdInvalidError:
+            pass
+        except Exception as er:
+            err = format_exc()
+            LOGS.exception(er)
+
+            me = await asst.get_me()
+
+            def error_text():
+                return f"**#ERROR #INLINE**\n\nQuery: `{me.username} {pattern}`\n\n**Traceback:**\n`{err}`"
+
             try:
-                await func(event)
+                await event.answer(
+                    [
+                        await event.builder.article(
+                            title="Unhandled Exception has Occured!",
+                            text=error_text(),
+                            buttons=Button.url(
+                                "Report", "https://t.me/SarkarTerminal"
+                            ),
+                        )
+                    ]
+                )
             except QueryIdInvalidError:
-                pass
+                LOGS.exception(err)
             except Exception as er:
-                err = format_exc()
-
-                def error_text():
-                    return f"**#ERROR #INLINE**\n\nQuery: `{asst.me.username} {pattern}`\n\n**Traceback:**\n`{format_exc()}`"
-
                 LOGS.exception(er)
-                try:
-                    await event.answer(
-                        [
-                            await event.builder.article(
-                                title="Unhandled Exception has Occured!",
-                                text=error_text(),
-                                buttons=Button.url(
-                                    "Report", "https://t.me/SarkarTerminal"
-                                ),
-                            )
-                        ]
-                    )
-                except QueryIdInvalidError:
-                    LOGS.exception(err)
-                except Exception as er:
-                    LOGS.exception(er)
-                    await asst.send_message(udB.get_key("LOG_CHANNEL"), error_text())
+                await asst.send_message(udB.get_key("LOG_CHANNEL"), error_text())
 
-        asst.add_event_handler(wrapper, InlineQuery(pattern=pattern, **kwargs))
+    asst.add_event_handler(wrapper, InlineQuery(pattern=pattern, **kwargs))
 
-    return don
+return don
+
